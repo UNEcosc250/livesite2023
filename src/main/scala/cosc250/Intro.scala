521f4a449e0119dd83332b4d7085e12d7ce7d950
William Billingsley <wbillingsley@cantab.net>
2024-02-26 12:03:03 +1100

    Update title page for 2024

--- src/main/scala/cosc250/Intro.scala
+++ src/main/scala/cosc250/Intro.scala
@@ -6,9 +6,10 @@ val frontPage = div(
   div(^.cls := "lead",
     markdown.div(
       """
-        |# COSC250 Live sessions 2023
+        |# COSC250 Live sessions 2024
         |
-        |This is a site for holding the slides for the live sessions in 2023.
+        |This is a site for holding the slides for the live sessions in 2024.
+        |
         |It'll update as trimester goes on (and will often look quite scratchy)
         |""".stripMargin
     ),
